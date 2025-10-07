b8a7d0bbd6c3b5fe2983c3aa19a34d5ec312c598
fawwazdev <fawwazotin@gmail.com>
2025-10-07 15:14:02 +0700

    update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (mysqli_connect_errno()) {
 // Inisialisasi variabel pencarian
 $cari = "";
 
-// Jika form pencarian diki
+// Jika form pencarian dikirim
 if (isset($_GET['cari'])) {
     $cari = $_GET['cari'];
     $query = "SELECT * FROM barang WHERE nama_barang LIKE '%$cari%'";
